620971b45cc8b37cfe40878a198c01b6de0c1fee
nghilt7 <nghi.tanlam@gmail.com>
2024-07-23 19:39:24 +0700

    release v1

---
+++ src/components/Options.js
@@ -0,0 +1,26 @@
+function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          disabled={hasAnswered}
+          key={option}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Options;
---
+++ src/components/ResetButton.js
@@ -0,0 +1,5 @@
+function ResetButton() {
+  return;
+}
+
+export default ResetButton;
